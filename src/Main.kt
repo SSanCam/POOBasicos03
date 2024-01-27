14e20e34915674c5924963ab42721ecf7a4f0f6f
Sara S Camilleri <ssancam391@g.educaand.es>
2024-01-27 22:45:18 +0100

    apartados 2/7

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,80 @@
+/**
+ * Función principal (main) que crea instancias de la clase Persona y realiza comparaciones entre ellas.
+ */
 fun main() {
+
+    //------------- PERSONA 1 --------------------
+    val persona1 = Persona(65.80, 1.75)
+    println("Introduce un nombre para Persona1: ")
+    val nombre = readln()
+    persona1.nombre = nombre
+
+    //IMPRIMIMOS LOS DATOS DE PERSONA-1
+    val infoP1 = (
+        "Persona-1\n" +
+                "Nombre: ${persona1.nombre}\n" +
+                "Peso: ${persona1.peso}kg\n" +
+                "Altura: ${persona1.altura}m"
+    )
+    println(infoP1)
+    println()
+    //----------- FIN PERSONA 1-------------------
+
+    //------------- PERSONA 3 --------------------
+    val persona3 = Persona(75.0, 1.80)
+    val p3Imc = persona3.imc(persona3.peso, persona3.altura)
+
+    //IMPRIMIMOS LOS DATOS DE PERSONA-3
+    val infoP3=(
+        "Persona-3\n" +
+                "Peso: ${persona3.peso}kg\n" +
+                "Altura: ${persona3.altura}m\n" +
+                "IMC: $p3Imc %"
+    )
+    println(infoP3)
+    println()
+    //----------- FIN PERSONA 3-------------------
+
+
+    //------------- PERSONA 2 --------------------
+    val persona2 = Persona(60.75, persona3.altura)
+    val p2Imc = persona2.imc(persona2.peso, persona2.altura)
+    val infoP2 = ("Persona-2\n" +
+                    "Peso: ${persona2.peso}\nkg" +
+                    "Altura: ${persona2.altura}m\n" +
+                    "IMC: $p2Imc")
+    println(infoP2)
+    println()
+    println(infoP3)
+    println()
+
+    val sonIguales = persona2 == persona3
+    if (sonIguales == false){
+        println("Las personas 2 y 3 no son iguales.")
+    }else{
+        println("Las personas 2 y 3 son iguales.")
+    }
+
+    //----------- FIN PERSONA 2-------------------
+
+    //MÉTODOS ACTIVIDAD-3
+
+    //saludar()
+    val saludo = Persona.saludar(persona1.nombre)
+    println()
+    println(saludo)
+    println()
+
+    //alturaEncimaMedia():Boolean
+    val alturas = listOf(persona1.altura, persona2.altura, persona3.altura )
+
+    for (altura in alturas){
+        if (Persona.alturaEncimaMedia(altura)){
+            println("La altura: $altura, está por encima de la media.")
+        }else{
+            println("La altura: $altura, no está dentro de la media.")
+        }
+    }
+
 
 }
---
+++ src/Persona.kt
@@ -0,0 +1,92 @@
+/**
+ * Clase que representa a una persona con atributos como peso, altura, nombre e índice de masa corporal (IMC).
+ *
+ * @property peso Peso de la persona en kilogramos.
+ * @property altura Altura de la persona en metros.
+ * @property imc Índice de Masa Corporal (IMC) calculado a partir del peso y la altura.
+ * @property nombre Nombre de la persona.
+ * @constructor Crea una instancia de la clase Persona con los atributos de peso y altura.
+ */
+class Persona(peso: Double, altura: Double) {
+
+    //GETTERS Y SETTERS - PROPIEDADES DE CLASE:
+    val peso: Double = peso
+
+    val altura: Double = altura
+
+    val imc = String.format("%.2f", peso / (altura * altura))
+
+    var nombre: String = ""
+        get() = field
+        set(value) {
+            require(value.isNotBlank()) { "Debes introducir un nombre válido." }
+            field = value
+        }
+
+
+    //CONSTRUCTOR SECUNDARIO
+    /**
+     * Constructor secundario que permite crear una instancia de Persona con nombre, peso y altura.
+     *
+     * @param nombre Nombre de la persona.
+     * @param peso Peso de la persona en kilogramos.
+     * @param altura Altura de la persona en metros.
+     */
+    constructor(nombre: String, peso: Double, altura: Double) : this(peso, altura) {
+        this.nombre = nombre
+    }
+//METODOS:
+    /**
+     * Método para calcular el Índice de Masa Corporal (IMC) de la persona.
+     *
+     * @param peso Peso de la persona en kilogramos.
+     * @param altura Altura de la persona en metros.
+     * @return Valor del IMC.
+     */
+    fun imc(peso: Double, altura: Double): Double {
+        val imc = peso / (altura * altura)
+        return imc
+    }
+
+    // MÉTODOS DE IGUALDAD Y HASHCODE QUE HEMOS GENERADO A PARTIR DEL CÓDIGO PREVIO
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Persona
+
+        if (peso != other.peso) return false
+        if (altura != other.altura) return false
+        if (imc != other.imc) return false
+        if (nombre != other.nombre) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = peso.hashCode()
+        result = 31 * result + altura.hashCode()
+        result = 31 * result + imc.hashCode()
+        result = 31 * result + nombre.hashCode()
+        return result
+    }
+
+    //COMPANION OBJECT
+    /**
+     * Método de clase para saludar a una persona por su nombre.
+     *
+     * @param nombre Nombre de la persona a saludar.
+     * @return Saludo.
+     */
+    companion object {
+        fun saludar(nombre: String): String {
+            return ("Hola, $nombre")
+        }
+
+        fun alturaEncimaMedia(altura: Double): Boolean {
+            return altura >= 1.75
+        }
+    }
+
+}
+
